669977ebeb8cb7bdd553b181de694451f0b7c221
Adel Moreno <adel.argenix@gmail.com>
2018-07-11 22:35:24 -0600

    Added content to Adam, model, and user case

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -6,3 +6,34 @@
  * Time: 4:40 PM
  */?>
 
+<html>
+	<body>
+		<p>users</p>
+		<ul>
+			<li>userId (primary key)</li>
+			<li>userEmail</li>
+			<li>userPassword</li>
+		</ul>
+		<p>poll</p>
+		<ul>
+			<li>doYouVotePoll (primary key)</li>
+			<li>doYouVoteUserId (foreign key)</li>
+			<li>doYouWantVote</li>
+			<li>doYouHaveVote</li>
+			<li>doYouHadVote</li>
+		</ul>
+		<p>Poll Results</p>
+		<ul>
+			<li>countUserId (foreign key)</li>
+			<li>countDoYouVotePoll (foreign key)</li>
+		</ul>
+		<p>Relations</p>
+		<ul>
+			<li>One poll can have many results (1 to n)</li>
+			<li>Many users can vote on many polls (m to n)</li>
+		</ul>
+		<nav>
+			<a href="index.php">Index</a>
+		</nav>
+	</body>
+</html>
--- epic/user-case.php
+++ epic/user-case.php
@@ -6,3 +6,32 @@
  * Time: 4:40 PM
  */?>
 
+<html>
+	<body>
+		<p><strong>Use Case&#58;</strong>Adam has decided that it is time to upgrade his phone. He is looking for an easy way to see the specifications for different mobile devices. He would also like to be able to do a side by side comparison. It would also be a plus if there was a polling system that shows how people want, have, or used to have the same model of a device. This way he could see which devices are very popular.</p>
+		<p><strong>Frustrations&#58;</strong></p>
+			<ul>
+				<li>Not having all of a phone specifications all on one page</li>
+				<li>Not being able to compare phones side by side</li>
+				<li>Not being able to see that raw data of a phone vs someone reviewing and telling him why he should like something. He wants to draw conclusions for himself.</li>
+				<li>Not being able to see how many other people have or want a specific mobile device</li>
+			</ul>
+		<p><strong>Interaction Flow&#58;</strong></p>
+			<ul>
+				<li>Adam navigates to website in his browser.</li>
+				<li>The server displays the homepage with a few articles on the most popular mobile devices today</li>
+				<li>Adam clicks on the link to set up a profile</li>
+				<li>The webpage displays a small popup to register with fields for his information</li>
+				<li>Adam fills out form with a username and password and clicks submit</li>
+				<li>Adam is redirected back to the homepage</li>
+				<li>Adam clicks on one that displays the Iphone X</li>
+				<li>Server displays webpage that shows all the specifications for iphone X as well as suggestions to compare other popular models</li>
+				<li>Adam review specifications and decides he wants the Iphone X and navigates to the poll on the webpage and clicks the want button</li>
+				<li>The server then adds +1 to the count of want</li>
+				<li>Adam closes browser and goes to purchase a new phone!</li>
+			</ul>
+		<nav>
+			<a href="index.php">Index</a>
+		</nav>
+	</body>
+</html>
